91ab73dbfb34a43e573f1bd618bcbd3016ba0d3c
Nisanth <nelang17@student.aau.dk>
2018-03-20 12:10:55 +0100

    Test

--- p2_ros_app/lib/pages/EventsPage.dart
+++ p2_ros_app/lib/pages/EventsPage.dart
@@ -6,10 +6,7 @@ class Events extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
         appBar: new AppBar(
-            title: new Text("List"), backgroundColor: roskildeOrange),
-        body: new Container(
-          child: new Center(
-              child: new Icon(Icons.list, size: 150.0, color: Colors.black)),
+
         ));
   }
 }
