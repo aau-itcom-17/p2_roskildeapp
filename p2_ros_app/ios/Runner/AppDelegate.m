a2da4189ac2bd4c294d2623133b1778cdb50e070
Anton <akroghp@gmail.com>
2018-03-22 16:14:37 +0100

    Login Implementation

--- p2_ros_app/ios/Runner/AppDelegate.m
+++ p2_ros_app/ios/Runner/AppDelegate.m
@@ -1,12 +1,13 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
 
-@implementation AppDelegate
+    @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+    - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+[GeneratedPluginRegistrant registerWithRegistry:self];
+// Override point for customization after application launch.
+    return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
 @end
+
--- p2_ros_app/lib/main.dart
+++ p2_ros_app/lib/main.dart
@@ -3,13 +3,16 @@ import 'package:p2_ros_app/pages/MapPage.dart' as first;
 import 'package:p2_ros_app/pages/EventsPage.dart' as second;
 import 'package:p2_ros_app/pages/MyEventsPage.dart' as third;
 import 'package:p2_ros_app/pages/CreateEventPage.dart' as fourth;
+import 'package:p2_ros_app/Login.dart' as login;
 
 Color roskildeOrange = const Color.fromARGB(255, 255, 115, 0);
 
 void main() {
-  runApp(new MaterialApp(
-    home: new MyTabs(),
-  ));
+  runApp(new login.MyApp());
+  /*MaterialApp(
+  home: new MyTabs(),
+  ));*/
+
 }
 
 class MyTabs extends StatefulWidget {
